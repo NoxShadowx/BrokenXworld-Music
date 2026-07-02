7576400a47443babef4b4f2cf8cf0604ba97a581
Nox <mrchikoo31@gmail.com>
2026-07-02 22:35:09 +0530

    Add initial Flask application in web.py
    Signed-off-by: Nox <mrchikoo31@gmail.com>

---
+++ anony/web.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "BrokenXworld Music Bot is Running!"
+
+def run():
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
+    )
